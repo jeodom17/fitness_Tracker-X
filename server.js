8b3e15ff1bddb7dc0a77d06b58192171f507799f
jeodom17 <jeodom17@gmail.com>
2021-11-10 14:38:44 -0500

    add server.js

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+
+const PORT = process.env.PORT || 3017;
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost:27017/workout",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  }
+);
+
+app.use(require("./routes"));
+
+app.listen(PORT, () => {
+  console.log(`Listening on ${PORT}!`);
+});
