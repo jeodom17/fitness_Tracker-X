6021f600c65a5599c25aa8b640ad144bc5b80b73
jeodom17 <jeodom17@gmail.com>
2021-11-10 19:16:46 -0500

    api routes-post

--- models/Workout.js
+++ models/Workout.js
@@ -1,17 +1,62 @@
 
+// const mongoose = require('mongoose');
+// const Schema = mongoose.Schema;
+
+// const WoSchema = new Schema();
+
+// WoSchema.add({
+//     day: {
+//         type: Date,
+//         default: Date.now,
+//       },
+//       exercises: Array,
+//     });
+
+
+
+// mongoose.model('Workout', WoSchema);
+
 const mongoose = require('mongoose');
+
 const Schema = mongoose.Schema;
 
-const WoSchema = new Schema();
-
-WoSchema.add({
-    day: {
-        type: Date,
-        default: Date.now,
+const workoutSchema = new Schema({
+  day: {
+    type: Date,
+    default: () => new Date(),
+  },
+  exercises: [
+    {
+      type: {
+        type: String,
+        trim: true,
+        required: 'Enter an exercise type',
       },
-      exercises: Array,
-    });
+      name: {
+        type: String,
+        trim: true,
+        required: 'Enter an exercise name',
+      },
+      duration: {
+        type: Number,
+        required: 'Enter an exercise duration in minutes',
+      },
+      weight: {
+        type: Number,
+      },
+      reps: {
+        type: Number,
+      },
+      sets: {
+        type: Number,
+      },
+      distance: {
+        type: Number,
+      },
+    },
+  ],
+});
 
+const Workout = mongoose.model('Workout', workoutSchema);
 
-
-mongoose.model('Workout', WoSchema);
+module.exports = Workout;
--- routes/api/workoutRoutes.js
+++ routes/api/workoutRoutes.js
@@ -1,4 +1,21 @@
 const router = require("express").Router();
+const { Workout } = require("../../models");
 const db = require("../../models");
 
-//* 
+//* match fetch calls in api.js
+
+router.get("/api/workouts", (req, res) => {
+    
+})
+
+
+
+router.post("/", async (req, res) => {
+    Workout.create(body)
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
